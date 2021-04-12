c90a01fd3baebf8bcab1f076b8c3ef10a145aa3e
Julien-Devos <julien.devos@student.uclouvain.be>
2021-04-12 15:58:35 +0200

    Changed text to flexbox and updated database creation

--- webSite/db_init.py
+++ webSite/db_init.py
@@ -2,7 +2,7 @@ import sqlite3 as sqlite
 from flask import current_app, g
 from flask.cli import with_appcontext
 from webSite.db_create import create_db
-import click
+import click, os
 
 
 def init_db():
@@ -10,10 +10,14 @@ def init_db():
     create_db(db)
 
 
-@click.command('init-db')
+# Initialisation de la commande $ flask create-db
+@click.command('create-db')
 @with_appcontext
 def init_db_command():
-    """Clear the existing data and create new tables."""
+    """Clear the existing data and create the database"""
+    if os.path.exists(current_app.config['DATABASE']):
+        click.echo('The database already exists, the file has been overwritten')
+        os.remove(current_app.config['DATABASE'])
     init_db()
     click.echo('The database has been successfully filled with the data !')
 
@@ -21,8 +25,7 @@ def init_db_command():
 def get_db():
     if 'db' not in g:
         g.db = sqlite.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite.PARSE_DECLTYPES
+            current_app.config['DATABASE']
         )
         g.db.row_factory = sqlite.Row
 
